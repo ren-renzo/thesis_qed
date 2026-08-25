7eb2d065323a419ed208bb2dc1634ccae9db9b5e
Keyel Luna <kimlourenluna.dev@gmail.com>
2026-08-26 00:16:38 +0800

    cors updated

--- server.js
+++ server.js
@@ -25,10 +25,16 @@ app.use(logger);
 
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cors());
+
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // kung gumagamit ka ng cookies/session
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true})) //this will allow to read the url body tags
 
